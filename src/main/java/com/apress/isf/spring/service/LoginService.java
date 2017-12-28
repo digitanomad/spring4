48f703e5c2ffa7e901248e4e1702198a280282a3
digitanomad <digitanomad@gmail.com>
2017-12-28 18:30:13 +0900

    CH06 - 리소스 파일 사용하기

---
+++ src/main/java/com/apress/isf/spring/service/LoginService.java
@@ -0,0 +1,35 @@
+package com.apress.isf.spring.service;
+
+import com.apress.isf.java.service.Login;
+
+public class LoginService implements Login {
+	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public boolean isAuthorized(String email, String pass) {
+		if (username.equals(email) && password.equals(pass)) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}
